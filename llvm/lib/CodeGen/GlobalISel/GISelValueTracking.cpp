bfa1372b36cbcf5f9a2a237b97ca2fd5da69cb07
Alan Li <me@alanli.org>
2026-07-22 08:16:15 -0700

    [GlobalISel] Track nonzero through G_SEXT

--- llvm/lib/CodeGen/GlobalISel/GISelValueTracking.cpp
+++ llvm/lib/CodeGen/GlobalISel/GISelValueTracking.cpp
@@ -280,6 +280,9 @@ bool GISelValueTracking::isKnownNeverZero(Register R, const APInt &DemandedElts,
     break;
   }
 
+  case TargetOpcode::G_SEXT:
+    return isKnownNeverZero(MI.getOperand(1).getReg(), DemandedElts, Depth + 1);
+
   case TargetOpcode::G_SELECT:
     return isKnownNeverZero(MI.getOperand(2).getReg(), DemandedElts,
                             Depth + 1) &&
